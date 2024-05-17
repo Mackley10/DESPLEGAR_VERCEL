d2c3223e592b6426f16e711316ca518c8e664462
Neiter <neiter.aylas@unmsm.edu.pe>
2024-05-17 18:00:55 -0500

    Webserver

--- app.js
+++ app.js
@@ -316,7 +316,7 @@ app.listen(port, () => {
 })
 
 
-*/
+--
 
 //      Cragando las variables de entorno
 
@@ -362,5 +362,54 @@ app.listen(port, () => {
 })
 
 
+*/
 
+//      Agregando la pestaña salidas
+
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT;
+app.set('view engine', 'hbs'); // para usar el handlebars para express
+hbs.registerPartials(__dirname + '/views/partials'); //para usar parciales
+//para servir contenido estatico
+app.use(express.static('public')); //definicion de midelware express. si public esta en otro sitio especificar path
+
+app.get('/', (req, res) => {
+    res.render('home',{
+        nombre:'Armando David',
+        titulo: 'Curso de node'
+    });
+})
+
+
+app.get('/generic', (req, res) => {
+    res.render('generic',{
+        nombre:'Armando David',
+        titulo: 'Curso de node'
+    })
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements',{
+        nombre:'Armando David',
+        titulo: 'Curso de node'
+    })
+});
+
+app.get('/salidas',(req,res)=>{
+    res.render('salidas',{
+        nombre: 'Armando David',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listenining al http://localhost:${port}`)
+})
 
